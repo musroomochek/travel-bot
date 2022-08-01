e57118df6ada5d54b68043ed8c538daaf427f97b
Alex <sashamarx67@gmail.com>
2022-08-01 16:29:45 +0300

    добавил фильтр городов

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -2,6 +2,7 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 
+from data.cities import array
 from loader import dp
 from states.select_city import GetMessage
 
@@ -17,6 +18,11 @@ async def start_cmd(message: types.Message):
 @dp.message_handler(state=GetMessage.city)
 async def get_city_from_user(message: types.Message, state: FSMContext):
     city = message.text
-    await message.answer(f'Хорошо, твой город - <b>{city}</b>\n\n'
-                         f'Выбери на клавиатуре свой интерес.')
-    await state.reset_state()
+    if city.title() in array:
+        await message.answer(f'Хорошо, твой город - <b>{city.title()}</b>\n\n'
+                             f'Выбери на клавиатуре свой интерес.')
+        await state.reset_state()
+
+    else:
+        await message.answer('Такого города не существует.\n'
+                             'Попробуй еще раз.')
