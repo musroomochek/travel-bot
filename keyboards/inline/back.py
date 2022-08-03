46b5b9c44832ce2c8c2068b8a1e63c22ba6fa598
Alex <sashamarx67@gmail.com>
2022-08-03 15:00:27 +0300

    переименовал название кнопки (как переменную)

--- keyboards/inline/back.py
+++ keyboards/inline/back.py
@@ -1,12 +1,8 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
-hobby = InlineKeyboardMarkup(row_width=3,
-                             inline_keyboard=[
-                                 [
-                                     InlineKeyboardButton(text='назад', callback_data='back'),
-                                 ]
-                             ])
-
-
-
+go_back = InlineKeyboardMarkup(row_width=3,
+                               inline_keyboard=[
+                                   [
+                                       InlineKeyboardButton(text='назад', callback_data='back'),
+                                   ]
+                               ])
