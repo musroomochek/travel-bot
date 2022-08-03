40cd43fb218e68535abb8e05d200eab09c2db5eb
burenko <ivanburenko2004@gmail.com>
2022-08-03 15:44:35 +0300

    оставил только кнопки активного и пасивного отдыха

--- keyboards/inline/hobby_keyboard.py
+++ keyboards/inline/hobby_keyboard.py
@@ -2,16 +2,14 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 hobby = InlineKeyboardMarkup(row_width=3,
                              inline_keyboard=[
-                                 [
-                                     InlineKeyboardButton(text='Гулять', callback_data='walking'),
-                                 ],
-                                 [
-                                     InlineKeyboardButton(text='Музеи', callback_data='museums'),
-                                 ],
+
                                  [
                                      InlineKeyboardButton(text="активный отдых", callback_data='active'),
                                  ],
                                  [
                                      InlineKeyboardButton(text='спокойный отдых', callback_data='calm'),
+                                 ],
+                                 [
+                                     InlineKeyboardButton(text='назад', callback_data='back')
                                  ]
                              ])
