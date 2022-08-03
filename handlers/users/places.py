5a5e916bd158bede3707323f2d8f769105eaee8f
burenko <ivanburenko2004@gmail.com>
2022-08-03 15:45:20 +0300

    сделал кнопку назад

--- handlers/users/places.py
+++ handlers/users/places.py
@@ -2,21 +2,29 @@ from aiogram import types
 from aiogram.dispatcher.filters import Text
 
 from handlers.users.start import start_cmd
+from keyboards.inline.active_types import go_active
 from keyboards.inline.back import go_back
+from keyboards.inline.calm_types import go_calm
+from keyboards.inline.hobby_keyboard import hobby
 from loader import dp
 
 
-@dp.callback_query_handler(Text(equals='walking'))
-async def show_walking(call: types.CallbackQuery):
-    await call.message.edit_text('Супер! Вот места которые могут быть вам интересны!', reply_markup=go_back)
+@dp.callback_query_handler(Text(equals='active'))
+async def show_active(call: types.CallbackQuery):
+    await call.message.edit_text('Супер! Выберете вид актиного отдыха!', reply_markup=go_active)
 
 
-@dp.callback_query_handler(Text(equals='museums'))
-async def show_museums(call: types.CallbackQuery):
-    await call.message.edit_text('Вот музеи которые могут быть вам интересны!')
+@dp.callback_query_handler(Text(equals='calm'))
+async def show_calm(call: types.CallbackQuery):
+    await call.message.edit_text('Супер! Выберете вид спокойного отдыха!', reply_markup=go_calm)
 
 
 @dp.callback_query_handler(Text(equals='back'))
 async def go_back_in_menu(call: types.CallbackQuery):
     await call.message.delete()
     await start_cmd(message=call.message)
+
+
+@dp.callback_query_handler(Text(equals='in_menu'))
+async def in_menu(call: types.CallbackQuery):
+    await call.message.edit_text('Выбери на клавиатуре свой интерес.', reply_markup=hobby)
