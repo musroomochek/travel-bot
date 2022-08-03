051a4a7d75e4a77db524aca1bf3577b3eaf59973
Alex <sashamarx67@gmail.com>
2022-08-03 14:59:50 +0300

    добавил удаление приветственного сообщения

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -10,14 +10,28 @@ from states.select_city import GetMessage
 
 @dp.message_handler(Command('start'))
 async def start_cmd(message: types.Message):
-    await message.answer(f'<b>Привет, {message.from_user.first_name}!</b>\n\n'
-                         f'Нечем заняться на досуге? <b>Я тебе помогу!</b>\n\n'
-                         f'Для начала введи свой город :)')
-    await GetMessage.city.set()
+    msg = await message.answer(f'<b>Привет, {message.from_user.first_name}!</b>\n\n'
+                               f'Нечем заняться на досуге? <b>Я тебе помогу!</b>\n\n'
+                               f'Для начала введи свой город :)')
+    message_id = msg.message_id
+    state = dp.current_state(chat=message.chat.id, user=message.chat.id)
+    await state.set_state(GetMessage.city)
+    await state.update_data(
+        {
+            'message_id': message_id
+        }
+    )
 
 
 @dp.message_handler(state=GetMessage.city)
 async def get_city_from_user(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    message_id = data.get('message_id')
+    await dp.bot.delete_message(
+        chat_id=message.chat.id,
+        message_id=message_id
+    )
+    await message.delete()
     city = message.text
     if city.title() in array:
         await message.answer(f'Хорошо, твой город - <b>{city.title()}</b>\n\n'
