1d8eb1e6284075b9ef9b3548dec738b347223360
burenko <ivanburenko2004@gmail.com>
2022-08-03 14:36:22 +0300

    дописал одну реплику

--- handlers/users/places.py
+++ handlers/users/places.py
@@ -6,4 +6,10 @@ from loader import dp
 
 @dp.callback_query_handler(Text(equals='walking'))
 async def show_walking(call: types.CallbackQuery):
-    await call.message.edit_text('Супер! Вот ваши локации')
+    await call.message.edit_text('Супер! Вот места которые могут быть вам интерессны!')
+
+
+
+@dp.callback_query_handler(Text(equals='museums'))
+async def show_museums(call: types.CallbackQuery):
+    await call.message.edit_text('Вот музеи которые могут быть вам интерессны!')
