6c67b8f3a275355f4aa337042cb3c02136232fcd
Alex <sashamarx67@gmail.com>
2022-08-03 15:00:05 +0300

    добавил реакцию на нажатие кнопки назад

--- handlers/users/places.py
+++ handlers/users/places.py
@@ -1,14 +1,22 @@
 from aiogram import types
 from aiogram.dispatcher.filters import Text
 
+from handlers.users.start import start_cmd
+from keyboards.inline.back import go_back
 from loader import dp
 
 
 @dp.callback_query_handler(Text(equals='walking'))
 async def show_walking(call: types.CallbackQuery):
-    await call.message.edit_text('Супер! Вот места которые могут быть вам интересны!')
+    await call.message.edit_text('Супер! Вот места которые могут быть вам интересны!', reply_markup=go_back)
 
 
 @dp.callback_query_handler(Text(equals='museums'))
 async def show_museums(call: types.CallbackQuery):
     await call.message.edit_text('Вот музеи которые могут быть вам интересны!')
+
+
+@dp.callback_query_handler(Text(equals='back'))
+async def go_back_in_menu(call: types.CallbackQuery):
+    await call.message.delete()
+    await start_cmd(message=call.message)
