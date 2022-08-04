59261e04b93abfc8aa490ec423e61d24bc798db0
Alex <sashamarx67@gmail.com>
2022-08-04 17:24:04 +0300

    перенес константы в переменные из .env, сделал ссылку для подключения к БД

--- data/config.py
+++ data/config.py
@@ -8,3 +8,13 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа s
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 
+DATABASE = env.str('DATABASE')
+DB_USER = env.str('DB_USER')
+DB_PASSWORD = env.str('DB_PASSWORD')
+DB_NAME = env.str('DB_NAME')
+DB_PLUGIN = env.str('DB_PLUGIN')
+DB_HOST = env.str('DB_HOST')
+DB_PORT = env.str('DB_PORT')
+
+DB_LINK = f'{DATABASE}+{DB_PLUGIN}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
