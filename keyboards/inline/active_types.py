80aed6794bb667304e6e664bb1f7f496d62f4a7e
burenko <ivanburenko2004@gmail.com>
2022-08-17 15:21:52 +0300

    добавил два новых вида активного отдыха

--- keyboards/inline/active_types.py
+++ keyboards/inline/active_types.py
@@ -6,7 +6,7 @@ go_active = InlineKeyboardMarkup(row_width=3,
                                          InlineKeyboardButton(text='Прогулка', callback_data='go_walking_0'),
                                      ],
                                      [
-                                         InlineKeyboardButton(text='Физическая нагрузка', callback_data='go_physical_0'),
+                                         InlineKeyboardButton(text='Физическая нагрузка', callback_data='physical'),
                                      ],
                                      [
                                          InlineKeyboardButton(text='Назад', callback_data='in_menu')
